397fa6d606ff6fce2cdbc6f510201958f944e771
mr13 <mr13@DESKTOP-PN0DMCN>
2024-05-19 18:18:08 +0400

    SOLID Final

--- UML&Patterns 02. 4. Interface_Segregation/Program.cs
+++ UML&Patterns 02. 4. Interface_Segregation/Program.cs
@@ -1,33 +1,75 @@
 ﻿#region Interface Segregation Bad Example
+//abstract class Animal
+//{
+//    public abstract void Feed();
+//    public abstract void Groom();
+//}
+
+//class Dog : Animal
+//{
+//    public override void Feed()
+//    {
+//        Console.WriteLine("Dog Feed");
+//    }
+
+//    public override void Groom()
+//    {
+//        Console.WriteLine("Dog Groom");
+//    }
+//}
+
+//class Tiger : Animal
+//{
+//    public override void Feed()
+//    {
+//        Console.WriteLine("Tiger Feed");
+//    }
+
+//    public override void Groom()
+//    {
+//        Console.WriteLine("Tiger Groom");
+//    }
+//}
+
+#endregion
+
+#region Interface Segregation Good Example
 abstract class Animal
 {
     public abstract void Feed();
-    public abstract void Groom();
+}
+interface IGroom
+{
+    void Groom();
 }
 
-class Dog : Animal
+abstract class Pet : Animal, IGroom
+{
+    public void Groom()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class Dog : Pet
 {
     public override void Feed()
     {
         Console.WriteLine("Dog Feed");
     }
 
-    public override void Groom()
+    public  void Groom()
     {
         Console.WriteLine("Dog Groom");
     }
 }
 
-class Tiger : Animal
+abstract class Predator: Animal { }
+class Tiger : Predator
 {
     public override void Feed()
     {
         Console.WriteLine("Tiger Feed");
-    }
-
-    public override void Groom()
-    {
-        Console.WriteLine("Tiger Groom");
     }
 }
 
